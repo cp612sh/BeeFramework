26c45488c53c1f0889deeed923f1249f367acef4
cp612sh@gmail.com <MacChai@ChaiPeixideMac.local>
2014-09-23 19:34:58 +0800

    Initial Commit

---
+++ projects/HelloBee/HelloBee/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloBee
+//
+//  Created by ChaiPeixi on 14-9-23.
+//  Copyright (c) 2014年 Fuckmeagain.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FMPAppDelegate.h"
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FMPAppDelegate class]));
+    }
+}
---
+++ projects/HelloBee/HelloBee/view_iPad/AppBoard_iPad.h
@@ -0,0 +1,24 @@
+//
+//	 ______    ______    ______    
+//	/\  __ \  /\  ___\  /\  ___\   
+//	\ \  __<  \ \  __\_ \ \  __\_ 
+//	 \ \_____\ \ \_____\ \ \_____\ 
+//	  \/_____/  \/_____/  \/_____/ 
+//
+//	Powered by BeeFramework
+//
+//
+//  AppBoard_iPad.h
+//  HelloBee
+//
+//  Created by ChaiPeixi on 14-9-23.
+//  Copyright (c) 2014年 Fuckmeagain.org. All rights reserved.
+//
+
+#import "Bee.h"
+
+@interface AppBoard_iPad : BeeUIBoard
+
+AS_SINGLETON( AppBoard_iPad )
+
+@end
